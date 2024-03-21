93297903fe979b057ed8877e67fb51ac3a1343a8
Ola <olaa.kazmierska@gmail.com>
2024-03-21 10:32:17 +0100

    obracanie obrazu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -53,22 +57,77 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(50, 36);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(57, 29);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(50, 71);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 29);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(50, 106);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(67, 29);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(50, 141);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 5;
+            button2.Text = "Rotate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(570, 450);
+            Controls.Add(button2);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -18,5 +18,40 @@ namespace lab_4___wczytywanie_obrazu
         {
             pictureBox1_Click(this, EventArgs.Empty);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                if (radioButton1.Checked)
+                {
+                    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                }
+                else if (radioButton2.Checked)
+                {
+                    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                }
+                else if (radioButton3.Checked)
+                {
+                    pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                }
+                pictureBox1.Invalidate(); 
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+                
+        }
     }
 }
